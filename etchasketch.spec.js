e8312dec6036c93859e5e49da0132b40ee8f0db6
Clonky <clonky_cs@yahoo.de>
2022-09-22 19:38:44 +0200

    Setup jsdom testing

--- etchasketch.spec.js
+++ etchasketch.spec.js
@@ -1,3 +1,8 @@
+/**
+ * @jest-environment jsdom
+ */
+const generateRow = require("./etchasketch");
+
 test("Should check that proper row of divs is created", () => {
     const row = generateRow(10);
     expect(row.childNodes.length).toBe(10);
